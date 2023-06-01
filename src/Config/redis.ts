0ca80d4e55ae7013bb104818d0daa78b6b40712b
Barny Victor <victorbarny4@gmail.com>
2023-06-01 08:43:55 +0100

    removed redis

--- src/Config/redis.ts
+++ src/Config/redis.ts
@@ -1,17 +1,17 @@
-import { createClient } from "redis";
+// import { createClient } from "redis";
 
-const client = createClient({
-    socket: {
-        host: 'redis-14753.c262.us-east-1-3.ec2.cloud.redislabs.com',
-        port: 14753
-    }
-});
+// const client = createClient({
+//     socket: {
+//         host: 'redis-14753.c262.us-east-1-3.ec2.cloud.redislabs.com',
+//         port: 14753
+//     }
+// });
 
-(async () => {
-  await client.connect();
-})();
+// (async () => {
+//   await client.connect();
+// })();
 
-client.on("connect", () => console.log("Redis Client Connected"));
-client.on("error", (err) => console.log("Redis Client Connection Error", err));
+// client.on("connect", () => console.log("Redis Client Connected"));
+// client.on("error", (err) => console.log("Redis Client Connection Error", err));
 
-export default client;
+// export default client;
